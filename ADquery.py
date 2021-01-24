f65fc587655f38b12f9d637dfdd5a6c273f71432
Christopher Kyriacou <kyriacou96@gmail.com>
2021-01-23 23:50:55 -0500

    Add python scripts for AD auditing

---
+++ ADquery.py
@@ -0,0 +1,147 @@
+#Applied Research Associates
+#This class is designed to allow administrators to query information that are vital for cmmc compliance audits of active directory instances.
+
+from pyad import *
+import numpy as np
+import pyad.pyadutils
+from datetime import datetime
+
+class ADquery:
+    CN = ""
+    user = ""
+    unusedComputerCount = 0
+    unusedUserCount = 0
+    unusedUsers = np.array([])
+    unusedComputers = np.array([])
+    pwdLastSetNDays = np.array([])
+
+#This constructor initialzes an ADquery object and validates pyad's connection to AD by locating a user account by a passed common name.
+#Will add extra validation to this constructor for final product.
+    def __init__(self, CN):
+        if(CN != ""):
+            self.CN = CN
+            self.user = aduser.ADUser.from_cn(CN)
+        else:
+            raise ValueError("The Common Name cannot be null!")
+
+#Allows you to set a new cn and initialize a different user.
+    def set_CN(self, CN):
+        if (CN != ""):
+            self.CN = CN
+            self.user = aduser.ADUser.from_cn(CN)
+        else:
+            raise ValueError("The Common Name cannot be null!")
+
+#Finds the last time a list of users each last logged on to their accounts.
+    def get_last_login_users(self, array):
+        for i in array:
+            x = aduser.ADUser.from_cn(i)
+            print(i, x.get_last_login())
+
+"""
+Get a list of users that are within a container of a specific distinguished name nomenclature, are of a specific object category within that container, and have logged on before (can adjust this to find user accounts that have never been used as well).
+The generated list is designed to be passed into get_login_past_N_days special purpose function.
+"""
+    def get_last_user_login_list(self, dn, objCat):
+        con = pyad.adcontainer.ADContainer.from_dn(dn)
+        array = np.array([])
+        for obj in con.get_children():
+            c = obj.get_attribute("CN")
+            d = obj.get_attribute("objectCategory")
+            c = str(c[0])
+            d = str(d[0])
+            if (d == objCat):
+                array = np.append(array, c)
+        array2 = np.array([])
+        for i in array:
+
+            u = aduser.ADUser.from_cn(i)
+            e = u.get_attribute("CN")
+            e = str(e[0])
+
+            b = u.get_allowed_attributes()
+            if ('logonCount' in b):
+                v = u.get_attribute("logonCount")
+                g = v[0]
+                if (v[0] > 0):
+                    str(i)
+                    array2 = np.append(array2, e)
+        return array2
+
+#Find the users, within a provided list, that have not logged on in N days.
+    def get_login_past_N_days(self, N, array, type):
+        now = datetime.now()
+        dt = now.strftime("%Y-%m-%d %H:%M:%S")
+        currentDate = datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
+        array2 = np.array([])
+        for i in array:
+            x = aduser.ADUser.from_cn(i)
+            z = x.get_last_login()
+            z = str(z)
+            y = datetime.strptime(z, "%Y-%m-%d %H:%M:%S")
+            diff = abs((currentDate - y).days)
+            if(diff >= N):
+                array2 = np.append(array2, [i, diff])
+                if(type == "Computer"):
+                    self.unusedComputerCount += 1
+                    self.unusedComputers = np.append(self.unusedComputers, "{} Days Unused: {}".format(i, diff))
+                elif(type == "User"):
+                    self.unusedUserCount += 1
+                    self.unusedUsers = np.append(self.unusedUsers, "{} Days Unused: {}".format(i, diff))
+                else:
+                    raise ValueError("The value must be Computer or User!")
+
+#Find out when all users of a specific object category, within a container under a specific distinguished name type, last set their #password.
+    def get_pwd_last_login_N_days(self, dn, objectCategory, N):
+        container = adcontainer.ADContainer.from_dn(dn)
+        for obj in container.get_children():
+            now = datetime.now()
+            dt = now.strftime("%Y-%m-%d %H:%M:%S")
+            currentDate = datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
+            c = obj.get_attribute("CN")
+            d = obj.get_attribute("objectCategory")
+            c = str(c[0])
+            d = str(d[0])
+            e = aduser.ADUser.from_cn(c)
+            if (d == objectCategory):
+                f = e.get_password_last_set()
+                f = str(f)
+                y = datetime.strptime(f, "%Y-%m-%d %H:%M:%S")
+                diff = abs((currentDate - y).days)
+                if (diff >= N):
+                    self.pwdLastSetNDays = np.append(self.pwdLastSetNDays, c)
+
+#Return a report of the users and computers that have not logged in the last N days.
+    def get_unused_report(self):
+        message ="Unused Users: "
+        for i in self.unusedUsers:
+            message += ("{}, ").format(str(i))
+        message += ("\nUnused User Count: {}").format(self.unusedUserCount)
+        message += "\nUnused Computers: "
+        for i in self.unusedComputers:
+            message += ("{}, ").format(str(i))
+        message += ("\nUnused Computer Count: {}").format(self.unusedComputerCount)
+        return message
+
+#Return a report on the users that have not changed their password in N days.
+    def get_pwd_report(self):
+        message = "PWD Unchanged Past Day Limit: "
+        for i in self.pwdLastSetNDays:
+            message += ("{}, ").format(str(i))
+        return message
+
+#Print an overall message on information found by querying through Active Directory.
+    def toString(self):
+        print("Unused Users: ")
+        for i in self.unusedUsers:
+            print(i, ", ")
+        print("\nUnused User Count: ", self.unusedUserCount)
+        print("\nUnused Computers: ")
+        for i in self.unusedComputers:
+            print(i, ", ")
+        print("\nUnused Computer Count: ", self.unusedComputerCount)
+        print("\nPWD Unchanged Past Day Limit: ")
+        for i in self.pwdLastSetNDays:
+            print(("{}, ").format(str(i)))
+
+# ------------------------------------------------End of Class ADquery ---------------------------------------------------------------------------------------------
---
+++ Active_Directory_ARA.py
@@ -0,0 +1,65 @@
+#Applied Research Associates
+#This script utilizes the ADquery class to audit a variety of users, computers and groups for cmmc compliance
+import ADquery as ad
+import os
+import numpy as np
+
+
+
+#Use ADquery class to audit active directory for users that have not logged on in N days.
+
+def logon_info(CN, containers, objectCategories, types, N):
+    AD = ad.ADquery(CN)
+    count = 0
+    list = np.array([])
+    N = int(N)
+    while(count < len(containers)):
+        list = AD.get_last_user_login_list(containers[count], objectCategories[count])
+        AD.get_login_past_N_days(N, list, types[count])
+        count += 1
+    AD.toString()
+    doc = AD.get_unused_report()
+    f = open(("Logon_in_{}_days.txt").format(N), "w")
+    f.write(doc)
+    f = open(("Logon_in_{}_days.txt").format(N), "r")
+    print(f.read())
+    f.close()
+
+#Use ADquery to locate users that have not changed their password in N days.
+
+def last_set_pwd(CN, containers, objectCategories, N):
+    AD = ad.ADquery(CN)
+    count = 0
+    N = int(N)
+    while(count < len(containers)):
+        AD.get_pwd_last_login_N_days(containers[count], objectCategories[count], N)
+        count += 1
+    doc = AD.get_pwd_report()
+    f = open(("PWD_Last_Set_Past_{}_days.txt").format(N), "w")
+    f.write(doc)
+    f = open(("PWD_Last_Set_Past_{}_days.txt").format(N), "r")
+    print(f.read())
+    f.close()
+
+#Main method that takes in os variables from a bash file and passess them into the appropriate functions to audit Active Directory instance within the current admin user's domain
+
+def main():
+    CN = os.getenv('AD_USER')
+    containerUsers = os.getenv('CONTAINER_USERS')
+    containerComputers = os.getenv('CONTAINER_COMPUTERS')
+    usersObjectCategory = os.getenv('OBJECT_CATEGORY_USERS')
+    computersObjectCategory = os.getenv('OBJECT_CATEGORY_COMPUTERS')
+    containers = np.array([containerUsers, containerComputers])
+    containers2 = np.array([containerUsers])
+    objectCategories = np.array([usersObjectCategory, computersObjectCategory])
+    objectCategories2 = np.array([usersObjectCategory])
+    types = np.array(["User", "Computer"])
+    N = os.getenv('DAYS_UNUSED')
+    N2 = os.getenv('DAYS_LS')
+    logon_info(CN, containers, objectCategories, types, N)
+    last_set_pwd(CN, containers2, objectCategories2, N)
+
+
+if __name__ == "__main__":
+    main()
+
